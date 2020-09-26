b732cee0ea3ef5d8d64db6228c6c66df70870e00
KioFe <kingbean127@gmail.com>
2020-09-26 13:11:54 +0700

    update

---
+++ precache-manifest.9429ac95e399d076bd8b51a83960c0c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cf3ba3f6d3d86b51fb341ed8c304f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8561b78d9056d374e32a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a81c649cca735a355ff",
+    "url": "/static/js/2.8a21e50a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a21e50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8561b78d9056d374e32a",
+    "url": "/static/js/main.9e7d6852.chunk.js"
+  },
+  {
+    "revision": "5da8b8851d16edff1ba7",
+    "url": "/static/js/runtime-main.37aa84d2.js"
+  }
+]);
